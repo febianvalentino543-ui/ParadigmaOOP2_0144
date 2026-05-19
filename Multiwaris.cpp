d63df696424b6c8af34ab8299b89c4a7e982ae07
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-19 18:46:22 +0700

    menambahkan kelas budi yang mewarisi dari kelas pekerja dan pelajar

--- Multiwaris.cpp
+++ Multiwaris.cpp
@@ -26,3 +26,14 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) : 
+    orang(pUmur), 
+    pekerja(pUmur), 
+    pelajar(pUmur) {
+        cout << "budi dibuat\n " << umur << endl;
+    }
+};
+
