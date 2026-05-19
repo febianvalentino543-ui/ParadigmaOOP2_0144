2de26ae1273b0e2345f1d20731796de2cc933cc8
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-05-19 18:45:05 +0700

    menambah kelas pelajar

--- Multiwaris.cpp
+++ Multiwaris.cpp
@@ -18,3 +18,11 @@ public:
     }
 };
 
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : 
+    orang(pUmur) {
+        cout << "pelajar dibuat\n " << umur << endl;
+    }
+};
+
